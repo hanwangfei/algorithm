6c56b6cc4fb93bcb7c9e6130268a8c9f613b5185
hanwangfei <88571507+hanwangfei@users.noreply.github.com>
2021-11-05 12:23:11 +0800

    Add files via upload

---
+++ 07_贪心算法/main.cpp
@@ -0,0 +1,21 @@
+﻿//贪心算法：在某一个标准下，优先考虑最满足标本的样本，最后考虑最不满足标准的样本，最终得到一个答案的算法，叫做贪心算法
+//注意，很多情况下贪心算法要找的是优良解而不是最优解，因为很多情况局部最优无法推出整体最优
+#include"会议安排问题.h"
+void test()
+{
+	BestProgram b;
+	vector<Program> p;
+	p.push_back(Program(6, 7));
+	p.push_back(Program(6, 8));
+	p.push_back(Program(8, 13));
+	p.push_back(Program(14, 16));
+	p.push_back(Program(15, 16));
+	p.push_back(Program(17, 18));
+	cout << b.bestArrange(p, 6) << endl;
+	
+}
+int main()
+{
+	test();
+	return 0;
+}
---
+++ 07_贪心算法/会议安排问题.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+//有一间会议室，例如可使用时间从6.00到18.00，现有一堆会议，找出一个方案，使得可安排的会议尽可能多
+
+//贪心策略：结束时间越早越先安排
+class Program
+{
+public:
+	int start;
+	int end;
+	Program(int start, int end)
+	{
+		this->start = start;
+		this->end = end;
+	}
+	bool operator<(Program p)
+	{
+		return this->end < p.end;  //比较器
+	}
+};
+class BestProgram
+{
+public:
+	int bestArrange(vector<Program>& p, int timePoint, int endPoint=18)  //第一个参数为会议数组指针，第二个参数为当前时间节点,第三个参数为会议室关门时间
+	{
+		sort(p.begin(), p.end());
+		int result = 0;
+		for (int i = 0; i < p.size(); i++)
+		{
+			if (timePoint <= p[i].start && p[i].end <= endPoint)
+			{
+				result++;
+				timePoint = p[i].end;  //当前时间来到会议结束时间
+			} 
+		}
+		return result; 
+	}
+	
+};
+
+
